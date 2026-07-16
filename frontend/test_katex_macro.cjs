201754bd9dea435468e2bf871479bd4db99a96ec
smaxiso <smaxiso@users.noreply.github.com>
2026-07-16 17:06:57 +0530

    chore: remove test files

--- frontend/test_katex_macro.cjs
+++
@@ -1,7 +0,0 @@
-const katex = require('katex');
-try {
-  const html = katex.renderToString("\\lambda(y) = g(y)(1 + \\kappa g(y))", { throwOnError: true });
-  console.log("Success!");
-} catch(e) {
-  console.log("KaTeX Error:", e.message);
-}
